14efe510b0e278268e0013f881ce21281837791a
danielsouzza <daniel.lima.souzza@gmail.com>
2025-02-13 16:37:20 -0300

    fiat: search municipio

--- app/Http/Controllers/ControllerSchool.php
+++ app/Http/Controllers/ControllerSchool.php
@@ -11,10 +11,13 @@ class ControllerSchool extends Controller
     function index(Request $req)
     {
         $schools = Escola::query()->where(function ($query) {
-            if(request('search')){
-                $query->where('escola', 'like', '%' . request('search') . '%')
-                    ->orWhere('municipio', 'like', '%' . request('search') . '%');
+            if(request('search-school')){
+                $query->where('escola', 'like', '%' . request('search') . '%');
             }
+            if(request('search-municipio')){
+                $query->where('municipio', 'like', '%' . request('search') . '%');
+            }
+
         })->orderBy('escola')->paginate(15)->through(function ($escola) {
             $municipio_proximo = Municipio::query()->where('nome','like','%'. $escola->municipio.'%')->first();
             $escola->setAttribute('municipio_proximo',!!$municipio_proximo);
